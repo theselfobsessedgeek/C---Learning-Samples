1180cb9f2fda76458da03eb18297021fa01c6a61
theselfobsessedgeek <shubhamahlawatg@gmail.com>
2021-05-12 14:48:36 +0530

    another commit

---
+++ DSA/returnAllCodes.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include <string.h>
+using namespace std;
+
+int getCodes(string input, string output[10000]) {
+    /*
+    You are given the input text and output string array. Find all possible codes and store in the output string array. Don’t print the codes.
+    Also, return the number of codes return to the output string. You do not need to print anything.
+    */
+   
+}
